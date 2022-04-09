3833b58b00f1ebbfb28e0cf32de9ae22a001f93f
rewayod <97519521+rewayod@users.noreply.github.com>
2022-04-09 13:11:15 -0400

    Update stocks.py

--- app/stocks.py
+++ app/stocks.py
@@ -1,26 +1,15 @@
 
-
 print("STOCKS REPORT...")
 
-import os
-from dotenv import load_dotenv
-from pandas import read_csv
+from app.alphavantage_service import fetch_stocks_data
 
-from app.utils import to_usd
-load_dotenv()
+symbol = input("Please input a stock ticker (default: 'NFLX'): ") or "NFLX"
 
-ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
-
-symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
-url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
-
-df = read_csv(url)
-#print(df.columns)
-#breakpoint()
+df = fetch_stocks_data(symbol)
 
 latest = df.iloc[0]
 
 print(symbol)
 print(latest["timestamp"])
 print(latest["close"])
-print(to_usd(latest["close"]))
+print(to_usd((latest["close"])))
